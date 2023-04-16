639a220fbd79f4f3320b10af684fe62a8bd92d4e
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-04-16 17:39:15 +0000

    8

--- hash_substring.py
+++ hash_substring.py
@@ -8,13 +8,13 @@ def read_input():
     if "I" in ievade: # Ja ievadē ir burts "I"
         first_pattern = input().strip() # Ievada pirmo rindu, kas satur pattern jeb fragmentu
         second_text = input().strip() # Ievada otro rindu, kas satur text jeb tekstu
-        return (input().rstrip(), input().rstrip()) # Atgriež abas līnijas vienā
+        return (first_pattern, second_text) # Atgriež abas līnijas vienā
 
     if "F" in ievade: # Ja ievadē ir burts "F"
         with open("tests/06") as file: # Tiek atvērts fails "tests/06" un saglabāts kā file
             first_line = file.readline().strip()   # Nolasa pirmo rindu, kas satur pattern jeb fragmentu
             second_line = file.readline().strip()  # Nolasa otro rindu, kas satur text jeb tekstu
-        return (input().rstrip(), input().rstrip()) # Atgriež abas līnijas vienā    
+        return (first_pattern, second_text) # Atgriež abas līnijas vienā    
 
 
 def print_occurrences(output):
