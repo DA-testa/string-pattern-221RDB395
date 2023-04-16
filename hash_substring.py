ba704ecf5f8eafa700c86b234b032572996c063b
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-04-16 16:26:03 +0000

    try

--- hash_substring.py
+++ hash_substring.py
@@ -1,10 +1,24 @@
 # python3
+# 221RDB395 Anastasija Bondare 13.grupa
 
 def read_input():
+   
+    ievade = input()
+
+    if "I" in ievade: 
+        first_line = input().strip() 
+        second_line = input().strip()
+        
+
+    if "F" in ievade: 
+        ievade = "tests/06" 
+        with open(ievade, 'r') as file: 
+            first_line = file.readline().strip()  # pattern
+            second_line = file.readline().strip()  # text
+               
+
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
     # after input type choice
     # read two lines 
     # first line is pattern 
@@ -15,12 +29,30 @@ def read_input():
     # this is the sample return, notice the rstrip function
     return (input().rstrip(), input().rstrip())
 
-def print_occurrences(output):
+#def print_occurrences(output):
     # this function should control output, it doesn't need any return
-    print(' '.join(map(str, output)))
+    #print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
+    p = len(pattern)
+    t = len(text)
+
+    count = 0
+
+    for i in range(0,t-p+1):
+        found = True
+        for j in range (0, p):
+            if pattern[j] != text[i+j]:
+                found = False
+                break
+        if found:
+            count += 1
+    
+    if not count == 0:
+        print count
+        
+
 
     # and return an iterable variable
     return [0]
